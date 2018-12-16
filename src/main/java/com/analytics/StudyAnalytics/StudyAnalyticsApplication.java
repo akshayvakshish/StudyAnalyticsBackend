3f1efead347a7b11948380afa8622176dbe8994a
Akshay Vakshish <akshayvakshish>
2018-12-17 00:52:43 +0530

    first backend

---
+++ src/main/java/com/analytics/StudyAnalytics/StudyAnalyticsApplication.java
@@ -0,0 +1,14 @@
+package com.analytics.StudyAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAnalyticsApplication.class, args);
+	}
+
+}
+
