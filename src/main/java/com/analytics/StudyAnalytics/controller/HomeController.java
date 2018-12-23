850da0193ce01a05779fe583e3efa0121200a7e2
akshayvakshish <akshaykgr5454@gmail.com>
2018-12-24 03:03:07 +0530

    Interceptor, forms

--- src/main/java/com/analytics/StudyAnalytics/controller/HomeController.java
+++ src/main/java/com/analytics/StudyAnalytics/controller/HomeController.java
@@ -1,6 +1,4 @@
 package com.analytics.StudyAnalytics.controller;
-
-
 import com.analytics.StudyAnalytics.model.Course;
 import com.analytics.StudyAnalytics.model.Lead;
 import com.analytics.StudyAnalytics.model.Student;
@@ -9,39 +7,47 @@ import com.analytics.StudyAnalytics.service.LeadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class HomeController {
     @Autowired
     CourseService courseService;
+    //@Autowired
+    private Lead lead= new Lead();
 
     @Autowired
     LeadService leadService;
-
+    @CrossOrigin(value="http://localhost:4200")
     @RequestMapping(value = "/test",method = RequestMethod.GET)
-    public Student student(@RequestParam(value="name") String name, @RequestParam(value="age") Integer age){
-        System.out.println(name+"-"+age);
+//    public List<Course> student1(@RequestParam(value="name") String name, @RequestParam(value="age") Integer age){
+//        System.out.println(name+"-"+age);
+    public List<Course> student1(){
+
+        List<Course> c =new ArrayList<>();
         try {
-            List<Course> c = courseService.getAllCourses();
+            c = courseService.getAllCourses();
             c.forEach((course) -> System.out.println(course.getCourseName()));
         }
         catch (Exception e){
             System.out.println("Some exception occured!");
         }
-        return new Student("Akshay","10",24);
+        return c;
     }
-
+    @CrossOrigin(value="http://localhost:4200")
     @RequestMapping(value = "/posting",method = RequestMethod.POST)
-    public Student student(@RequestBody Lead lead){
+    public Lead student2(@RequestBody Lead lead){
         System.out.println(lead.getId()+"-"+lead.getCompanyName());
+
         try {
-            Lead c = leadService.addLead(lead);
-            System.out.println(c.getCompanyName());
+            lead = leadService.addLead(lead);
+            System.out.println(lead.getCompanyName());
         }
         catch (Exception e){
             System.out.println("Some exception occured!");
         }
-        return new Student("Daksh","10",20);
+        System.out.println("called"+lead);
+        return lead;
     }
 }
